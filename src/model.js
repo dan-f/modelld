5c4447b607c622d2450ac5e91f9d4f0744724280
Daniel Friedman <dfriedman58@gmail.com>
2016-08-11 15:59:28 -0400

    fixup! Clean up and document after implementing diffing

--- src/model.js
+++ src/model.js
@@ -138,7 +138,6 @@ export class Model {
    *   }
    */
   _diff (rdf) {
-    // TODO: refactor
     const diffMap = this._fields
       .toArray()
       .reduce((reduction, cur) => [...reduction, ...cur])
